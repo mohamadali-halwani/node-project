bed7bfaffcfbb7e57b648c29e947250b4bc273f0
MohamadAli Halwani <68155980+mohamadali-halwani@users.noreply.github.com>
2025-08-21 14:02:16 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+ import { render, screen } from '@testing-library/react';
+ import App from './App';
+ 
+-test('renders learn react link', () => {
++test('renders LinkedIn link', () => {
+   render(<App />);
+-  const linkElement = screen.getByText(/learn react/i);
+-  expect(linkElement).toBeInTheDocument();
++  const link = screen.getByText(/LinkedIn/i);
++  expect(link).toBeInTheDocument();
+ });
